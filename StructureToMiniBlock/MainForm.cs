a6c6cc7ce355ea685f085a39b890113ddcb9f86f
Non0reo <nolhan.pieroni@gmail.com>
2021-10-24 23:31:49 +0200

    Solved drag and drop issues

--- StructureToMiniBlock/MainForm.cs
+++ StructureToMiniBlock/MainForm.cs
@@ -36,29 +36,41 @@ namespace StructureToMiniBlock
 
         private void MainForm_DragDrop(object sender, DragEventArgs e)
         {
-			
-            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
-            if (tempo == 0)
-            {
-				if (files.Length > 1){
-					MessageBox.Show("Please put only one  file at the time");
-				} else {
-					foreach (var file in files){
-						//MessageBox.Show(file);
-						App.Struture.Structure structFunction = new App.Struture.Structure();
-						structFunction.Launch(file);
-						CreateForm secondForm = new CreateForm();
-						secondForm.Show();
+
+			string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+			if (tempo == 0)
+			{
+				if (/*_openFileDialog.ShowDialog() == DialogResult.OK &&*/ StructureToMiniBlock.Controls.MainMenuStrip.canOpenFile == true)
+				{
+
+
+					if (files.Length > 1)
+					{
+						MessageBox.Show("Please put only one file at the time");
 					}
-					var strip = new Controls.MainMenuStrip();
-					strip.openFile(false);
-					this.AllowDrop = true;
+					else
+					{
+						foreach (var file in files)
+						{
+							//MessageBox.Show(file);
+							this.AllowDrop = false;
+							App.Struture.Structure structFunction = new App.Struture.Structure();
+							structFunction.Launch(file);
+							CreateForm secondForm = new CreateForm();
+							secondForm.Show();
+						}
+						var strip = new Controls.MainMenuStrip();
+						strip.openFile(false);
+						this.AllowDrop = true;
+					}
+					
 				}
+				else MessageBox.Show("You can't open a file. Close the 'Make your Struture' window");
 				tempo = 1;
-			} else {
-				tempo = 0;
-            }
-			
+			}
+			else tempo = 0;
+
+
 		}
 
         private void MainForm_DragEnter(object sender, DragEventArgs e)
